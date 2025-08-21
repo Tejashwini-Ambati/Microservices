ebb25d8983d451d36936c4f08c8295f133c712e3
Tejashwini-Ambati <tejashwinicjo@gmail.com>
2025-08-21 23:49:37 +0530

    Update app.py

--- services/user/app.py
+++ services/user/app.py
@@ -1,6 +1,13 @@
-FROM python:3.10-slim
-WORKDIR /app
-COPY requirements.txt .
-RUN pip install --no-cache-dir -r requirements.txt
-COPY . .
-CMD ["python", "app.py"]
+from flask import Flask
+app = Flask(__name__)
+
+@app.route('/')
+def home():
+    return "Dont just exist, live"
+
+@app.route('/health')
+def health():
+    return "OK"
+
+if __name__ == "__main__":
+    app.run(host='0.0.0.0', port=5001)
